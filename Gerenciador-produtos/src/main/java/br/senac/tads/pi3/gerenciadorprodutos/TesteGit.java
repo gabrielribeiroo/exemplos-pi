427bd0e78014567dacf05761d1173e6a51780cea
gabrelvital49@gmail.com <gabriel.rvital@CAS555487W1029.senacsp.edu.br>
2019-08-16 22:04:10 -0300

    Correção de espaços em branco

--- Gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
+++ Gerenciador-produtos/src/main/java/br/senac/tads/pi3/gerenciadorprodutos/TesteGit.java
@@ -15,8 +15,8 @@ public class TesteGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("     Teste Git");
-        System.out.println("MAKE LOVE");
+        System.out.println("Teste Git");
+        System.out.println("MAKE nothing");
     }
     
 }
